ddd6e18e36b10a0cf3c91df449c0b6749c5bf00c
Viswa_workshop <Viswa_workshop@dachavaram>
2017-03-10 20:20:37 +0000

    TDD test class

---
+++ test/com/viswa/tdd/shopping/cart/ShoppingCartTest.java
@@ -0,0 +1,25 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.viswa.tdd.shopping.cart;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author Viswa_workshop
+ */
+public class ShoppingCartTest {
+    
+    public ShoppingCartTest() {
+    }
+    
+    
+    // TODO add test methods here.
+    // The methods must be annotated with annotation @Test. For example:
+    //
+    // @Test
+    // public void hello() {}
+}
