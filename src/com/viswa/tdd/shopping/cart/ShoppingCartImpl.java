123e8c5363af4815af58ed675ae676a76ad033fc
Viswa_workshop <Viswa_workshop@dachavaram>
2017-03-10 20:44:49 +0000

    TDD test class

--- src/com/viswa/tdd/shopping/cart/ShoppingCartImpl.java
+++ src/com/viswa/tdd/shopping/cart/ShoppingCartImpl.java
@@ -1,6 +1,6 @@
 /*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
+ * This is a Shoppingcart Impl class
+ * 
  */
 package com.viswa.tdd.shopping.cart;
 
@@ -21,6 +21,13 @@ public class ShoppingCartImpl {
     private static final String APPLE_CODE = "Apple";
     private static final String ORANGE_CODE = "Orange";   
    
+   /**
+    * This method is for calculating the item along with offer
+    * buy3 get 2 
+    * @param itemsCost
+    * @return value
+    * 
+    */ 
    public int apply2(Integer[] itemsCost) {
 
         int numberOfItems = itemsCost.length;
@@ -34,7 +41,13 @@ public class ShoppingCartImpl {
             return (numberOfItems %3)* itemsCost[0];
     }
    
-   
+    /**
+    * This method is for calculating the item along with offer
+    * buy1 get 1 
+    * @param itemsCost
+    * @return value
+    * 
+    */ 
    public int apply1(Integer[] itemsCost) {
         int numberOfItems = itemsCost.length;
         System.out.println("no of items: "+numberOfItems);
@@ -44,6 +57,10 @@ public class ShoppingCartImpl {
        return (numberOfItems /2)* itemsCost[0] + (numberOfItems %2)* itemsCost[0];
     }
    
+     /*
+      * This method is used for printing the Receipt.
+      * 
+      */
      public String printReceipt(List<String> shoppingCart) {
 
         BigDecimal total = new BigDecimal(scan(shoppingCart) * .01);
@@ -51,6 +68,11 @@ public class ShoppingCartImpl {
         return NumberFormat.getCurrencyInstance().format(total);
     }
    
+     /*
+      * This method is used for adding calculation
+      * List shoppingCart contains the Apples and oranges
+      * 
+      */
    private int scan(List<String> shoppingCart) {
         System.out.println("shoppingCart "+shoppingCart);
         int total = 0;
@@ -58,6 +80,9 @@ public class ShoppingCartImpl {
         int apples = frequency(shoppingCart, APPLE_CODE);
         total += this.apply1(nCopies(apples, APPLE_PRICE).toArray(new Integer[apples]));
         
+        int oranges = frequency(shoppingCart, ORANGE_CODE);
+        total += this.apply2(nCopies(oranges, ORANGE_PRICE).toArray(new Integer[oranges]));
+
         return total;
     }
 }
--- test/com/viswa/tdd/shopping/cart/ShoppingCartTest.java
+++ test/com/viswa/tdd/shopping/cart/ShoppingCartTest.java
@@ -1,6 +1,6 @@
 /*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
+ * This class is the ShoppingCart test class
+ * 
  */
 package com.viswa.tdd.shopping.cart;
 
